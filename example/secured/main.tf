0409b4675e82d676938eceb126406bc56d92fc7b
Anmol Nagpal <ianmolnagpal@gmail.com>
2023-07-28 16:57:38 +0530

    [fix/docdb_structure]Fix: Removed the default secuirty group created with document module and restructure the module.

--- example/secured/main.tf
+++ example/secured/main.tf
@@ -3,15 +3,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 module "vpc" {
   source  = "clouddrove/vpc/aws"
   version = "1.3.0"
