1507e29946f5dd33d0f862aae8a974247a563401
anmolnagpal <anmol@clouddrove.com>
2026-02-12 16:34:08 +0000

    fix(style): remove personal managedby naming from module text

--- variables.tf
+++ variables.tf
@@ -162,7 +162,7 @@ variable "label_order" {
 variable "managedby" {
   type        = string
   default     = "hello@clouddrove.com"
-  description = "ManagedBy, eg 'CloudDrove' or 'AnmolNagpal'"
+  description = "ManagedBy, eg 'CloudDrove'"
 }
 
 variable "deletion_protection" {
